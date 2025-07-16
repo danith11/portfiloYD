ec72803c34515eb4b03bdd38d9e30224b7ce2ebb
Yethum Danith <yethumdanith2002@gmail.com>
2025-07-16 21:18:55 +0530

    Add footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,14 @@ import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between fixed top-0 left-0 w-full items-center px-6 md:px-16 lg:px-36 py-5 ">
+    <div className="flex justify-between z-999 overflow-hidden fixed w-full items-center px-6 md:px-16 lg:px-36 py-5 ">
       <div className="cursor-pointer">
         <a href="/">
           <p className="font-bold text-3xl">Yethum Danith</p>
         </a>
       </div>
 
-      <div className="flex gap-20 items-center cursor-pointer">
+      <div className="flex flex-col gap-20 items-center cursor-pointer sm:flex-row">
         <a className="font-semibold text-xl" href="#home">
           Home
         </a>
