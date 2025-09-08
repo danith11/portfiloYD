f6b6db0c13bbd4adf26366b0aaddd0672434f90a
Yethum Danith <yethumdanith2002@gmail.com>
2025-09-08 17:46:16 +0530

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     { name: "Contact", path: "/Contact" },
   ];
   return (
-    <div className="flex justify-between z-999 overflow-hidden fixed w-full items-center px-6 md:px-16 lg:px-36 py-5 ">
+    <div className="flex justify-between z-999 overflow-hidden fixed w-full items-center px-6 md:px-16 lg:px-36 py-5 bg-white ">
       <div className="cursor-pointer">
         <a href="/">
           <p className="font-bold text-3xl">Yethum Danith</p>
@@ -22,7 +22,7 @@ const Navbar = () => {
           <a
             key={index}
             href={item.path}
-            className="text-white hover:text-slate-300 duration-300 transition-colors ease-in-out"
+            className="text-black hover:text-slate-600 duration-300 transition-colors ease-in-out"
           >
             {item.name}
           </a>
