1c1c17c1bd4c0431754f9d894a02f8775fd60d9e
Yethum Danith <yethumdanith2002@gmail.com>
2025-09-08 17:46:54 +0530

    Home Page updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import React from "react";
+import TypeWriter from "typewriter-effect";
 
 const Home = () => {
   return (
-    <div>
-      <div>
-        <p>Full Stack/Web Dveloper</p>
+    <div className="flex items-center justify-between gap-10 p-50">
+      <div className="flex-1">
+        <p className="text-6xl font-medium mb-5">Full-Stack / Web Developer</p>
+
+        {/* Reserve space so layout won’t shift */}
+        <div className="min-h-[60px]">
+          <TypeWriter
+            options={{
+              strings: [
+                "<p class='font-light text-3xl'>Hi, I am <span class='font-bold text-slate-700 text-3xl'>Yethum Danith</span></p>",
+              ],
+              autoStart: true,
+              loop: true,
+              delay: 75,
+              cursor: "", // hide cursor
+            }}
+          />
+        </div>
+
+        <p className="mt-4 text-m text-slate-800 max-w-xl">
+          A passionate Full-Stack Web Developer and lifelong learner who loves
+          building clean, user-friendly, and impactful web applications. I enjoy
+          turning ideas into reality through code and continuously exploring new
+          technologies to grow as a developer.
+        </p>
+
+        <div className="flex gap-4 my-5">
+          <button className="p-5 px-10 bg-slate-800 rounded-4xl text-slate-200 cursor-pointer hover:scale-102 duration-200 hover:text-slate-50">My Projects</button>
+          <button className="p-5 px-10 bg-slate-800 rounded-4xl text-slate-200 cursor-pointer hover:scale-102 duration-200 hover:text-slate-50">Download CV</button>
+        </div>
+      </div>
+
+      {/* Image */}
+      <div className="flex-1 flex justify-center">
+        <img
+          src="/src/assets/boy.jpg"
+          alt="boy"
+          className="rounded-full w-80 h-80 object-cover shadow-lg"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
