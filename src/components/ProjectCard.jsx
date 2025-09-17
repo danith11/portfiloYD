70bf45c0e3e8ef680e1f0db0fdaf20d33fd12b02
Yethum Danith <yethumdanith2002@gmail.com>
2025-09-17 22:11:55 +0530

    projects added

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const ProjectCard = () => {
   return (
-    <div>ProjectCard</div>
-  )
-}
+    <div>
+      <div className="justify-center items-center text-center">
+        <p className="text-2xl">Edwin Platform</p>
+        <p>
+          Developed an e-tutoring platform that connects students with tutors
+          for live interactive learning sessions. Designed a database schema to
+          support user roles like students, tutors, and admins. Integrated Zoom
+          API for seamless online classes and AWS S3 for secure file storage,
+          ensuring a smooth and reliable experience.
+        </p>
+        <div className="list-none flex gap-2 justify-center items-center">
+          <li className="border-2 p-2 border-slate-200">NextJS</li>
+          <li className="border-2 p-2 border-slate-200">Tailwind CSS</li>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ProjectCard
+export default ProjectCard;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
-    <div className='px-50 mt-20'>
+    <div className="px-50 mt-20">
       <div>
         <p>Projects</p>
+        <ProjectCard />{" "}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
