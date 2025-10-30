36aac60b7ef63560e3c1ef74d45265d727532320
Yethum Danith <yethumdanith2002@gmail.com>
2025-10-30 11:42:45 +0530

    Redesign home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,17 +4,19 @@ import BlurCircle from "./BlurCircle";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-between gap-10 px-50 pt-50">
+    <div className="flex flex-col-reverse lg:flex-row items-center justify-center gap-10 px-6 md:px-16 lg:px-36 pt-28 md:pt-36">
       <BlurCircle top="150px" left="100px" />
 
-      <div className="flex-1">
-        <p className="text-6xl font-medium mb-5">Full-Stack / Web Developer</p>
+      <div className="lg:flex-1">
+        <p className="text-4xl md:text-6xl font-medium mb-5">
+          Full-Stack / Web Developer
+        </p>
 
         <div className="min-h-[60px]">
           <TypeWriter
             options={{
               strings: [
-                "<p class='font-light text-3xl'>Hi, I am <span class='font-bold text-slate-700 text-3xl'>Yethum Danith</span></p>",
+                "<p class='font-light text-2xl md:text-3xl'>Hi, I am <span class='font-bold text-slate-700 text-2xl md:text-3xl'>Yethum Danith</span></p>",
               ],
               autoStart: true,
               loop: true,
@@ -31,21 +33,21 @@ const Home = () => {
           technologies to grow as a developer.
         </p>
 
-        <div className="flex gap-4 my-5">
-          <button className="p-5 px-10 bg-slate-800 rounded-4xl text-slate-200 cursor-pointer hover:scale-102 duration-200 hover:text-slate-50">
+        <div className="flex flex-col sm:flex-row gap-4 my-5">
+          <button className="py-3 px-6 bg-slate-800 rounded-xl text-slate-200 cursor-pointer hover:scale-102 duration-200 hover:text-slate-50">
             My Projects
           </button>
-          <button className="p-5 px-10 bg-slate-800 rounded-4xl text-slate-200 cursor-pointer hover:scale-102 duration-200 hover:text-slate-50">
+          <button className="py-3 px-6 bg-slate-800 rounded-xl text-slate-200 cursor-pointer hover:scale-102 duration-200 hover:text-slate-50">
             Download CV
           </button>
         </div>
       </div>
 
-      <div className="flex-1 flex justify-center">
+      <div className="lg:flex-1 flex justify-center items-center">
         <img
-          src="/src/assets/boy.jpg"
+          src="/src/assets/boy.jpeg"
           alt="boy"
-          className="rounded-full w-80 h-80 object-cover shadow-lg"
+          className="rounded-full w-72 h-72 md:w-80 md:h-80 object-cover shadow-lg"
         />
       </div>
     </div>
@@ -53,3 +55,59 @@ const Home = () => {
 };
 
 export default Home;
+
+// import React from "react";
+// import TypeWriter from "typewriter-effect";
+// import BlurCircle from "./BlurCircle";
+
+// const Home = () => {
+//   return (
+//     <div className="flex items-center justify-between gap-10 px-50 pt-50">
+//       <BlurCircle top="150px" left="100px" />
+
+//       <div className="flex-1">
+//         <p className="text-6xl font-medium mb-5">Full-Stack / Web Developer</p>
+
+//         <div className="min-h-[60px]">
+//           <TypeWriter
+//             options={{
+//               strings: [
+//                 "<p class='font-light text-3xl'>Hi, I am <span class='font-bold text-slate-700 text-3xl'>Yethum Danith</span></p>",
+//               ],
+//               autoStart: true,
+//               loop: true,
+//               delay: 75,
+//               cursor: "",
+//             }}
+//           />
+//         </div>
+
+//         <p className="mt-4 text-m text-slate-800 max-w-xl">
+//           A passionate Full-Stack Web Developer and lifelong learner who loves
+//           building clean, user-friendly, and impactful web applications. I enjoy
+//           turning ideas into reality through code and continuously exploring new
+//           technologies to grow as a developer.
+//         </p>
+
+//         <div className="flex gap-4 my-5">
+//           <button className="p-5 px-10 bg-slate-800 rounded-4xl text-slate-200 cursor-pointer hover:scale-102 duration-200 hover:text-slate-50">
+//             My Projects
+//           </button>
+//           <button className="p-5 px-10 bg-slate-800 rounded-4xl text-slate-200 cursor-pointer hover:scale-102 duration-200 hover:text-slate-50">
+//             Download CV
+//           </button>
+//         </div>
+//       </div>
+
+//       <div className="flex-1 flex justify-center">
+//         <img
+//           src="/src/assets/boy.jpg"
+//           alt="boy"
+//           className="rounded-full w-80 h-80 object-cover shadow-lg"
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     { name: "Contact", path: "/Contact" },
   ];
   return (
-    <div className="flex justify-between z-999 overflow-hidden fixed w-full items-center px-6 md:px-16 lg:px-36 py-5 bg-white ">
+    <div className="flex justify-between z-999 overflow-hidden  sm:fixed w-full items-center px-6 md:px-16 lg:px-36 py-5 bg-white ">
       <div className="cursor-pointer">
         <a href="/">
           <p className="font-bold text-3xl">Yethum Danith</p>
