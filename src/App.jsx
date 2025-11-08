7b6e6a6b7233439d4d5003b06a906057ac0825b6
Yethum Danith <yethumdanith2002@gmail.com>
2025-11-08 12:23:51 +0530

    CV download added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,55 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
-import TechStack from "./components/TechStack";
 import About from "./components/About";
 import Projects from "./components/Projects";
 
-
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <Home/>
-      <TechStack/>
-      <About/>
-      <Projects/>
-      <Footer/>
-    </>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+            </>
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Footer />} />{" "}
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 export default App;
+
+// import React from "react";
+// import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import Home from "./components/Home";
+// import TechStack from "./components/TechStack";
+// import About from "./components/About";
+// import Projects from "./components/Projects";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// const App = () => {
+//   return (
+//     <>
+//       <Navbar />
+//       <Home/>
+//       <TechStack/>
+//       <About/>
+//       <Projects/>
+//       <Footer/>
+//     </>
+//   );
+// };
+
+// export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,27 +5,38 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const navLinks = [
     { name: "Home", path: "/" },
-    { name: "About Me", path: "/About" },
-    { name: "Projects", path: "/Projects" },
-    { name: "Contact", path: "/Contact" },
+    { name: "About Me", path: "/about" },
+    { name: "Projects", path: "/projects" },
+    { name: "Contact", path: "/contact" },
   ];
   return (
     <div className="flex justify-between z-999 overflow-hidden  sm:fixed w-full items-center px-6 md:px-16 lg:px-36 py-5 bg-white ">
       <div className="cursor-pointer">
-        <a href="/">
+        <Link
+          onClick={() => {
+            scrollTo(0, 0);
+            // setIsOpen(false);
+          }}
+          to={"/"}
+        >
           <p className="font-bold text-3xl">Yethum Danith</p>
-        </a>
+        </Link>
       </div>
 
       <div className="flex flex-col gap-20 items-center cursor-pointer sm:flex-row">
         {navLinks.map((item, index) => (
-          <a
+          <Link
             key={index}
-            href={item.path}
+            to={item.path}
+            onClick={() => {
+            scrollTo(0, 0);
+            setIsOpen(false);
+          }}
+          
             className="text-black hover:text-slate-600 duration-300 transition-colors ease-in-out"
           >
             {item.name}
-          </a>
+          </Link>
         ))}
       </div>
     </div>
