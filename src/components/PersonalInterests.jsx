786a4db542c73b78bffacc826f3a748e43752951
Yethum Danith <yethumdanith2002@gmail.com>
2025-11-18 14:25:40 +0530

    image loading issue solved

--- src/components/PersonalInterests.jsx
+++ src/components/PersonalInterests.jsx
@@ -1,22 +1,25 @@
 import React from "react";
+import photography from "/src/assets/photography.jpg";
+import hike from "/src/assets/hike.jpg";
+import music from "/src/assets/music.jpg";
 
 const PersonalInterests = () => {
   const interests = [
     {
       name: "Photography",
-      image: "/src/assets/photography.jpg",
+      image: { photography },
       description:
         "Capturing moments and perspectives through the lens. I specialize in landscape and street photography, always looking for unique compositions and lighting that tell a story",
     },
     {
       name: "Hiking",
-      image: "/src/assets/hike.jpg",
+      image: { hike },
       description:
         "Exploring nature and challenging myself on various trails. There's something profoundly rewarding about reaching a summit and taking in the panoramic views after a challenging hike.",
     },
     {
       name: "Music",
-      image: "/src/assets/music.jpg",
+      image: { music },
       description:
         "Finding inspiration and relaxation through various genres. I enjoy creating playlists that match my mood and enhance productivity while coding or studying",
     },
@@ -24,9 +27,17 @@ const PersonalInterests = () => {
   return (
     <div className="flex flex-col md:flex-row gap-5 mt-8">
       {interests.map((item, index) => (
-        <div key={index} className="items-center text-center justify-center border border-slate-300 rounded-lg hover:shadow-2xl transition-all duration-500">
+        <div
+          key={index}
+          className="items-center text-center justify-center border border-slate-300 rounded-lg hover:shadow-2xl transition-all duration-500"
+        >
           <div>
-            <img src={item.image} alt="" srcset="" className="w-full h-60 object-cover rounded-tr-lg rounded-tl-lg" />
+            <img
+              src={item.image}
+              alt=""
+              srcset=""
+              className="w-full h-60 object-cover rounded-tr-lg rounded-tl-lg"
+            />
           </div>
           <div>
             <p className="text-lg font-medium mt-2"> {item.name}</p>
--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,24 +1,35 @@
 import React from "react";
 
+import html from "../assets/html.png";
+import css from "../assets/css.png";
+import js from "../assets/javascript.png";
+import reactLogo from "../assets/react.png";
+import nextjs from "../assets/nextjs.png";
+import node from "../assets/node.png";
+import python from "../assets/python.png";
+import tailwind from "../assets/tailwind.png";
+import ts from "../assets/typescript.png";
+import mongo from "../assets/mongodb.png";
+
 const TechStack = () => {
   const images = [
-    "/src/assets/html.png",
-    "/src/assets/css.png",
-    "/src/assets/javascript.png",
-    "/src/assets/react.png",
-    "/src/assets/nextjs.png",
-    "/src/assets/node.png",
-    "/src/assets/python.png",
-    "/src/assets/tailwind.png",
-    "/src/assets/typescript.png",
-    "/src/assets/mongodb.png",
+    html,
+    css,
+    js,
+    reactLogo,
+    nextjs,
+    node,
+    python,
+    tailwind,
+    ts,
+    mongo,
   ];
 
   return (
     <div className="px-50 mt-30">
       <div className="flex flex-col justify-center items-center">
-        <p className="font-bold text-4xl ">Tech Stack</p>
-        <div className="flex flex-wrap  gap-10 mt-10">
+        <p className="font-bold text-4xl">Tech Stack</p>
+        <div className="flex flex-wrap gap-10 mt-10">
           {images.map((item, index) => (
             <img
               key={index}
@@ -34,3 +45,41 @@ const TechStack = () => {
 };
 
 export default TechStack;
+
+
+// import React from "react";
+
+// const TechStack = () => {
+//   const images = [
+//     "/src/assets/html.png",
+//     "/src/assets/css.png",
+//     "/src/assets/javascript.png",
+//     "/src/assets/react.png",
+//     "/src/assets/nextjs.png",
+//     "/src/assets/node.png",
+//     "/src/assets/python.png",
+//     "/src/assets/tailwind.png",
+//     "/src/assets/typescript.png",
+//     "/src/assets/mongodb.png",
+//   ];
+
+//   return (
+//     <div className="px-50 mt-30">
+//       <div className="flex flex-col justify-center items-center">
+//         <p className="font-bold text-4xl ">Tech Stack</p>
+//         <div className="flex flex-wrap  gap-10 mt-10">
+//           {images.map((item, index) => (
+//             <img
+//               key={index}
+//               src={item}
+//               alt={`tech-${index}`}
+//               className="w-16 h-16 rounded-full hover:scale-105 transition duration-200 hover:shadow-2xl"
+//             />
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default TechStack;
